6c907448d0c7d33611fd8ced6679fe0e2c43c427
Joe Haddad <timer150@gmail.com>
2020-01-20 14:11:28 -0500

    Adjust Formatting Settings

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const Octokit = require('@octokit/rest')
+const Octokit = require('@octokit/rest');
 const octokit = new Octokit({
   auth: process.env.GH_TOKEN,
-})
+});
 
-const REPOSITORY_SETTINGS = { owner: 'zeit', repo: 'next.js' }
+const REPOSITORY_SETTINGS = { owner: 'zeit', repo: 'next.js' };
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
@@ -14,24 +14,26 @@ async function main() {
     ...REPOSITORY_SETTINGS,
     base: 'canary',
     state: 'open',
-  })
+  });
 
-  let readyForMerge = (await Promise.all(
-    all.data
-      .filter(k => k.labels.find(k => k.id === 1468772558))
-      .map(k => k.number)
-      .map(number =>
-        octokit.pulls.get({
-          ...REPOSITORY_SETTINGS,
-          pull_number: number,
-        })
-      )
-  ))
+  let readyForMerge = (
+    await Promise.all(
+      all.data
+        .filter(k => k.labels.find(k => k.id === 1468772558))
+        .map(k => k.number)
+        .map(number =>
+          octokit.pulls.get({
+            ...REPOSITORY_SETTINGS,
+            pull_number: number,
+          })
+        )
+    )
+  )
     .map(pr => pr.data)
-    .filter(pr => pr.maintainer_can_modify)
+    .filter(pr => pr.maintainer_can_modify);
 
   if (!readyForMerge.length) {
-    return false
+    return false;
   }
 
   async function mergeSinglePullRequest() {
@@ -39,27 +41,27 @@ async function main() {
       pr =>
         pr.mergeable &&
         (pr.mergeable_state === 'clean' || pr.mergeable_state === 'unstable')
-    )
+    );
     if (mergeable) {
-      console.log(`Merging PR ${mergeable.number}`)
+      console.log(`Merging PR ${mergeable.number}`);
       await octokit.pulls.merge({
         ...REPOSITORY_SETTINGS,
         pull_number: mergeable.number,
         merge_method: 'squash',
-      })
+      });
 
-      readyForMerge = readyForMerge.filter(pr => pr.number !== mergeable.number)
+      readyForMerge = readyForMerge.filter(
+        pr => pr.number !== mergeable.number
+      );
 
-      await sleep(5000)
-      return true
+      await sleep(5000);
+      return true;
     } else {
       readyForMerge.forEach(pr => {
         console.log(
-          `PR ${pr.number} (${pr.title}) state / mergeable: ${
-            pr.mergeable
-          } :: mergeable_state: ${pr.mergeable_state}`
-        )
-      })
+          `PR ${pr.number} (${pr.title}) state / mergeable: ${pr.mergeable} :: mergeable_state: ${pr.mergeable_state}`
+        );
+      });
     }
   }
 
@@ -73,49 +75,49 @@ async function main() {
           })
           .then(pr => pr.data)
       )
-    )
+    );
   }
 
-  let minutes = 10
+  let minutes = 10;
   while (readyForMerge.length && minutes-- >= 0) {
     console.log(
       `remaining prs: ${readyForMerge
         .map(pr => pr.number)
         .join(', ')} at ${new Date()}`
-    )
+    );
 
     await Promise.all(
       readyForMerge
         .filter(pr => pr.mergeable_state === 'behind')
         .map(pr => {
-          console.log(`Updating branch for PR: ${pr.number}`)
+          console.log(`Updating branch for PR: ${pr.number}`);
           return octokit.pulls.updateBranch({
             ...REPOSITORY_SETTINGS,
             pull_number: pr.number,
-          })
+          });
         })
-    )
+    );
 
-    const didAMerge = await mergeSinglePullRequest()
+    const didAMerge = await mergeSinglePullRequest();
     if (didAMerge) {
-      return true
+      return true;
     }
 
-    await sleep(1000 * 60) // one minute
-    await updatePullRequests()
-    console.log()
+    await sleep(1000 * 60); // one minute
+    await updatePullRequests();
+    console.log();
   }
 
-  return true
+  return true;
 }
 
 async function loopMain() {
   for (;;) {
-    const res = await main()
+    const res = await main();
     if (!res) {
-      break
+      break;
     }
   }
 }
 
-loopMain()
+loopMain();
